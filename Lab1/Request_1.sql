b0c7ef14cc18d41f9f5dc3cfca96d4ff6b0338c2
sofiiapodopryhora <sofiia.podopryhora@infiz.khpi.edu.ua>
2024-09-17 22:19:56 +0300

    Request_1.sql

--- Lab1/Request_1.sql
+++ Lab1/Request_1.sql
@@ -1,4 +1,4 @@
--> Select id,name,population,region 
--> from cities 
--> order by population DESC 
--> LIMIT 10 OFFSET 20;
+mysql> Select id,name,population,region 
+    -> from cities 
+    -> order by population DESC 
+    -> LIMIT 10 OFFSET 20;
